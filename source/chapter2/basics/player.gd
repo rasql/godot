62b06309e8fd1198207af9a877495d92ecb1a951
Raphael Holzer <raphael.holzer@citycable.ch>
2026-04-04 22:16:40 +0200

    updated intro to Godot 4.6.1

--- source/chapter2/basics/player.gd
+++ source/chapter2/basics/player.gd
@@ -1,31 +1,47 @@
-@tool
 extends RigidBody3D
 
+@export var mouse_sensitivity := 0.001
 @export var speed: float = 50.0
 @export var jump_impulse: float = 5.0
+		
+@onready var twist_pivot: Node3D = $TwistPivot
+@onready var pitch_pivot: Node3D = $TwistPivot/PitchPivot
 
-func _physics_process(_delta: float) -> void:
-	# 1. Handle Continuous Movement
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	# Note: In 3D, 'up' on a 2D vector usually maps to negative Z (forward)
-	var force = Vector3(direction.x, 0, direction.y) * speed
-	
-	# apply_central_force should be called in _physics_process for smooth motion
-	apply_central_force(force)
+var twist_input := 0.0
+var pitch_input := 0.0
 
 	
 func _input(event: InputEvent) -> void:
-	# 2. Handle Discrete Actions (Jumping)
 	if event.is_action_pressed("jump"):
-		# Using a smaller impulse or checking if grounded is usually best
 		apply_central_impulse(Vector3.UP * jump_impulse)
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_backward")
+		
+	apply_central_force(twist_pivot.basis * input * speed)
+	
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x = clamp(
+		pitch_pivot.rotation.x,
+		deg_to_rad(-30),
+		deg_to_rad(30),
+	)
+	twist_input = 0
+	pitch_input = 0
+		
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			twist_input = -event.relative.x * mouse_sensitivity
+			pitch_input = -event.relative.y * mouse_sensitivity
